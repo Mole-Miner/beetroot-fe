7e3191b29d47ab434a5fd1cdb3bc2e56fea3a900
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-06-23 00:37:57 +0300

    updated random color generator

--- css_grid/practice/live/app.js
+++ css_grid/practice/live/app.js
@@ -1,6 +1,6 @@
-const gridChildren = [...document.querySelectorAll(`[id|='example'] > section`)];
+const gridChildren = document.querySelectorAll(`[id|='example'] > section`);
 
-gridChildren.forEach(child => {
+for (const child of gridChildren) {
   const color = Math.floor(Math.random() * 16777215).toString(16);
   child.style.backgroundColor = `#${color}`;
-});
+}
