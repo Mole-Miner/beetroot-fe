96e9bb965bf4aaa22292f882272cc103f06f02bb
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-07-14 11:33:16 +0300

    move card flip script to separate file

--- css_animations/card_deck/scripts/card-deck.js
+++ css_animations/card_deck/scripts/card-deck.js
@@ -14,34 +14,6 @@ function generateCardDeck(cardDeckSelector = '.card-deck', cardClass = '.card') 
     }
 }
 
-function getRandomCard() {
-    const lastCard = 35;
-    return Math.round(Math.random() * lastCard);
-}
-
-function flipCards({frontAnimationName = 'rotate-front', backAnimationName = 'rotate-back'}, cardSelector = '.card') {
-    const cardDeck = document.querySelectorAll(cardSelector);
-    let previousCard = getRandomCard();
-    setInterval(() => {
-        let currentCard = getRandomCard();
-        while (previousCard === currentCard) {
-            currentCard = getRandomCard();
-        }
-        previousCard = currentCard;
-        const card = cardDeck.item(previousCard);
-        const cardFront = card.querySelector(`${cardSelector}__front`);
-        const cardBack = card.querySelector(`${cardSelector}__back`);
-        cardFront.style.animationName = frontAnimationName;
-        cardBack.style.animationName = backAnimationName;
-        const timeout = setTimeout(() => {
-            cardFront.style.animationName = null;
-            cardBack.style.animationName = null;
-            clearTimeout(timeout);
-        }, 4000);
-    }, 2000);
-}
-
 generateCardDeck('#scss-card-deck', 'scss-card-flip');
 generateCardDeck('#js-card-deck', 'js-card-flip');
-flipCards({frontAnimationName: 'rotate-front', backAnimationName: 'rotate-back'}, '.js-card-flip');
 
