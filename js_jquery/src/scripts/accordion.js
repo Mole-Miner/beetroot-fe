290348b6628f16ad97119ebdb2957d1d4562b5d7
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-08-17 20:58:43 +0300

    live examples

--- js_jquery/src/scripts/accordion.js
+++ js_jquery/src/scripts/accordion.js
@@ -2,27 +2,40 @@ import '@styles/styles.css';
 import '@styles/accordion.css';
 
 const products = Array
-    .from({length: 4})
-    .map((_, i) => ({
+    .from({length: 8}, (_, i) => ({
         name: `Product ${i + 1}`,
         description: `Product ${i + 1} description`
     }));
 
 const accordion = $('<section/>', {
     class: 'accordion'
-}).appendTo(document.body);
+})
+    .appendTo(document.body);
 
 function AccordionItem(data) {
-    const item = $('<div/>', {class: 'accordion-item'});
-    const itemTitle = $('<div/>', {class: 'accordion-item__title', text: data.name});
-    const itemArrow = $('<span />', {class: 'accordion-item__arrow', text: '>'}).appendTo(itemTitle);
-    const itemContent = $('<div/>', {class: 'accordion-item__content', text: data.description});
+    const item = $('<div/>', {
+        class: 'accordion-item'
+    });
+    const itemTitle = $('<div/>', {
+        class: 'accordion-item__title',
+        text: data.name
+    });
+    const itemArrow = $('<span/>', {
+        class: 'accordion-item__arrow',
+        text: '>'
+    })
+        .appendTo(itemTitle);
+    const itemContent = $('<div/>', {
+        class: 'accordion-item__content',
+        text: data.description
+    });
     item.append(itemTitle, itemContent);
     itemTitle.on('click', (e) => {
         e.preventDefault();
+        e.stopImmediatePropagation();
+        itemContent.slideToggle();
         itemTitle.toggleClass('accordion--active');
         itemArrow.toggleClass('accordion--active');
-        itemContent.slideToggle();
     });
     return item;
 }
@@ -30,4 +43,3 @@ function AccordionItem(data) {
 products.forEach((product) => {
     AccordionItem(product).appendTo(accordion);
 });
-
--- js_jquery/src/scripts/modal.js
+++ js_jquery/src/scripts/modal.js
@@ -13,6 +13,44 @@ const modal = $('<section/>', {
     .appendTo(modalOverlay);
 
 $('<button/>', {
+    text: 'Close',
+    class: 'modal__close'
+})
+    .appendTo(modal)
+    .on('click', () => {
+        modalOverlay.removeClass('active');
+    });
+
+$('<button/>', {
+    text: 'Open'
+})
+    .appendTo(document.body)
+    .on('click', () => {
+        modalOverlay.addClass('active');
+    });
+
+
+
+
+
+
+
+
+
+
+
+/*const modalOverlay = $('<section/>', {
+    class: 'modal-overlay'
+})
+    .appendTo(document.body);
+
+const modal = $('<section/>', {
+    text: 'Modal',
+    class: 'modal'
+})
+    .appendTo(modalOverlay);
+
+$('<button/>', {
     text: 'Close'
 })
     .appendTo(modal)
@@ -28,5 +66,5 @@ $('<button/>', {
     .on('click', function () {
         modalOverlay.addClass('active');
         modal.addClass('active');
-    });
+    });*/
 
