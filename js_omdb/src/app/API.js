ccf1c99884d4cf5f10815292b85f13e0cd0aea10
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-08-23 15:22:43 +0300

    movies pagination

--- js_omdb/src/app/API.js
+++ js_omdb/src/app/API.js
@@ -1,23 +1,26 @@
-export async function getMovies({search, type}) {
-    try {
-        const searchParams = new URLSearchParams({
-            s: search,
-            type,
-            i: 'tt3896198',
-            apikey: '6fcb550a',
-        }).toString();
-        const url = new URL(`http://www.omdbapi.com/?${searchParams}`);
-        const response = await fetch(url);
-        const {Search, totalResults} = await response.json();
-        return {
-            movies: Search ?? [],
-            total: Number(totalResults) ?? 0,
+export const moviesService = {
+    async getMovies({search, type, page}) {
+        try {
+            const searchParams = new URLSearchParams({
+                s: search,
+                type,
+                page,
+                i: 'tt3896198',
+                apikey: '6fcb550a',
+            }).toString();
+            const url = new URL(`http://www.omdbapi.com/?${searchParams}`);
+            const response = await fetch(url);
+            const {Search, totalResults} = await response.json();
+            return {
+                movies: Search ?? [],
+                total: Number(totalResults) ?? 0,
+            }
+        } catch (err) {
+            console.log(err);
+            return {
+                movies: [],
+                total: 0,
+            };
         }
-    } catch (err) {
-        console.log(err);
-        return {
-            movies: [],
-            total: 0,
-        };
     }
 }
--- js_omdb/src/app/omdb.js
+++ js_omdb/src/app/omdb.js
@@ -1,33 +1,114 @@
-import { getMovies } from '@js/API';
+import { moviesService } from '@js/API';
 
-async function renderMovies({movies, total}) {
-    const moviesSection = document.querySelector('main > section.movies');
-
-    if (movies.length === 0) {
-        moviesSection.textContent = 'Movie not found';
-        return;
-    }
-
-    movies.forEach(({Title}) => {
+const movieComponent = {
+    async render({Title, Year, Poster}) {
         const movieSection = document.createElement('section');
-        movieSection.textContent = Title;
-        moviesSection.appendChild(movieSection);
-    });
-}
+        movieSection.className = 'movie'
+        const moviePoster = document.createElement('img');
+        moviePoster.src = Poster;
+        const movieTitle = document.createElement('h1');
+        movieTitle.textContent = Title;
+        const movieYear = document.createElement('span');
+        movieYear.textContent = Year;
+        movieSection.append(moviePoster, movieTitle, movieYear);
+        return movieSection;
+    }
+};
 
-async function bootstrap() {
-    const form = document.querySelector('form.search');
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const formData = Object.fromEntries(new FormData(form));
-        if (!formData.search || !formData.type) {
+const moviesComponent = {
+    section: document.querySelector('section.movies'),
+    async render(movies) {
+        if (this.section.children.length > 0) {
+            Array.from(this.section.children).forEach((movieSection) => movieSection.remove());
+        }
+        if (movies.length === 0) {
+            this.section.textContent = 'Movie not found';
             return;
         }
-        const data = await getMovies(formData);
-        await renderMovies(data);
-        form.reset();
-    });
-}
+        for (const movie of movies) {
+            const component = await movieComponent.render(movie);
+            this.section.appendChild(component);
+        }
+    }
+};
 
-bootstrap()
-    .catch((err) => console.log('OMDB failed', err));
+const searchComponent = {
+    form: document.querySelector('form.search'),
+    searchOptions: {},
+    patchSearchOptions(options) {
+        this.searchOptions = {...this.searchOptions, ...options};
+    },
+    onSearch() {
+        this.form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const formData = Object.fromEntries(new FormData(this.form));
+            if (!formData.search || !formData.type) {
+                return;
+            }
+            this.patchSearchOptions({
+                ...formData,
+                page: 1,
+            })
+            const moviesData = await moviesService.getMovies(this.searchOptions);
+            paginationComponent.init({total: moviesData.total, page: moviesData.movies.length});
+            await Promise.all([
+                moviesComponent.render(moviesData.movies),
+                paginationComponent.render(),
+            ]);
+            this.form.reset();
+        });
+    }
+};
+
+const paginationComponent = {
+    section: document.querySelector('section.pagination'),
+    previousBtn: document.querySelector('button.pagination__previous'),
+    nextBtn: document.querySelector('button.pagination__next'),
+    currentSpan: document.querySelector('span.pagination__current'),
+    firstPage: 0,
+    lastPage: 0,
+    currentPage: 0,
+    async onPrevious() {
+        if (this.currentPage === this.firstPage) {
+            return;
+        }
+        searchComponent.patchSearchOptions({
+            page: --this.currentPage
+        });
+        const moviesData = await moviesService.getMovies(searchComponent.searchOptions);
+        await Promise.all([
+            moviesComponent.render(moviesData.movies),
+            this.render(),
+        ]);
+    },
+    async onNext() {
+        if (this.currentPage === this.lastPage) {
+            return;
+        }
+        searchComponent.patchSearchOptions({
+            page: ++this.currentPage
+        });
+        const moviesData = await moviesService.getMovies(searchComponent.searchOptions);
+        await Promise.all([
+            moviesComponent.render(moviesData.movies),
+            this.render(),
+        ]);
+    },
+    init({total, page}) {
+        this.firstPage = 1;
+        this.currentPage = 1;
+        this.lastPage = Math.ceil(total / page);
+        this.previousBtn.addEventListener('click', async () => {
+            await this.onPrevious();
+        });
+        this.nextBtn.addEventListener('click', async () => {
+            await this.onNext();
+        });
+    },
+    async render() {
+        this.currentSpan.textContent = this.currentPage.toString();
+        this.section.style.display = 'flex';
+    }
+};
+
+searchComponent.onSearch();
