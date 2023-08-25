4be5f42cf3f6e7337180405475f40f2817c432eb
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-08-26 00:55:00 +0300

    added gulp project

---
+++ js_tools/gulp/src/scripts/app.js
@@ -0,0 +1,15 @@
+function test1() {
+    console.log('!!!');
+}
+
+function test2() {
+    console.log('!!!');
+}
+
+test1();
+test2();
+
+class Test {}
+
+const test = new Test;
+console.log(test);
