3b1d8f1f664bb720c5001c2647554dcc87f36c32
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-08-26 13:30:42 +0300

    init

---
+++ js_classes/src/index.js
@@ -0,0 +1,4 @@
+import '@scss/styles.scss';
+import '@scss/project.scss';
+import '@js/search';
+import '@js/navigation';
---
+++ js_classes/src/scripts/search.js
@@ -0,0 +1,13 @@
+function search() {
+    console.log('aa');
+}
+
+search();
+
+function computeSmth() {
+    for (let i = 0; i < 15; i++) {
+        console.log(i);
+    }
+}
+
+computeSmth();
